0e8d46714dff93d2ecf7f4bc4a09aa0f172ebd2c
Tim van Osch <timvosch@pollex.nl>
2020-04-01 18:32:55 +0200

    Create response object for consistency

---
+++ pkg/api/response.go
@@ -0,0 +1,45 @@
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+// Response is an API Response
+type Response struct {
+	Status         int
+	Body           map[string]interface{}
+	responseWriter http.ResponseWriter
+	useWrapper     bool
+}
+
+// NewResponse creates a new API Response
+func NewResponse(w http.ResponseWriter) *Response {
+	w.Header().Set("Content-Type", "application/json")
+	return &Response{
+		responseWriter: w,
+		useWrapper:     true,
+		Status:         http.StatusOK,
+	}
+}
+
+// Send the built response
+func (r *Response) Send() {
+	r.writeBody()
+}
+
+// writeBody send the JSON response
+func (r *Response) writeBody() {
+	w := r.responseWriter
+	var body map[string]interface{}
+
+	if r.useWrapper == true {
+		body = map[string]interface{}{
+			"status": r.Status,
+			"data":   r.Body,
+		}
+	} else {
+		body = r.Body
+	}
+	json.NewEncoder(w).Encode(body)
+}
