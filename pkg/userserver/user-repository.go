39379ff7b6dc2b3d157bae2a181906dabee84024
Tim van Osch <timvosch@pollex.nl>
2020-04-02 17:12:42 +0200

    Fix interface exports

--- pkg/userserver/user-repository.go
+++ pkg/userserver/user-repository.go
@@ -2,7 +2,7 @@ package userserver
 
 // UserRepository is the Data Abstraction Layer for the User Server
 type UserRepository interface {
-	getUserByEmail(email string) *User
-	getUserById(id int) *User
+	GetUserByEmail(email string) *User
+	GetUserByID(id int) *User
 	InsertUser(user User) (*User, error)
 }
