dc7bf9e87593388a826ae5b7f4901b3c76f20200
Tim van Osch <timvosch@pollex.nl>
2020-04-02 17:25:06 +0200

    Implement password hashing

--- pkg/userserver/models.go
+++ pkg/userserver/models.go
@@ -1,8 +1,19 @@
 package userserver
 
+import "golang.org/x/crypto/bcrypt"
+
 // User represent a system account
 type User struct {
-	ID       int
-	email    string
-	password string
+	ID       int    `json:"id"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+// SetPassword sets and hashes the given password
+func (u *User) SetPassword(password string) {
+	out, err := bcrypt.GenerateFromPassword([]byte(password), 10)
+	if err != nil {
+		//
+	}
+	u.Password = string(out)
 }
