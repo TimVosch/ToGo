46b845dda70e5aa3d82f94d5eccbe967dbe4c42f
Tim van Osch <timvosch@pollex.nl>
2020-04-06 19:40:31 +0200

    Refactor routes to use api.CTX

--- pkg/todoserver/routes.go
+++ pkg/todoserver/routes.go
@@ -10,72 +10,97 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func (s *TodoServer) handleHealthCheck() http.HandlerFunc {
+func (s *TodoServer) handleHealthCheck() api.HandlerFunc {
 	// Create response
-	return func(w http.ResponseWriter, r *http.Request) {
+	return func(ctx *api.CTX, next func()) {
 		body := map[string]interface{}{"healthy": true}
-		api.SendResponse(w, http.StatusOK, body, "Everything is O.K.")
+		api.SendResponse(ctx.W, http.StatusOK, body, "Everything is O.K.")
 	}
 }
 
-func (s *TodoServer) handleGetTodos() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func (s *TodoServer) handleGetTodos() api.HandlerFunc {
+	return func(ctx *api.CTX, next func()) {
 		todos := s.db.GetTodosForUser(0)
-		api.SendResponse(w, http.StatusOK, todos, "Returned all Todos for given user")
+		api.SendResponse(ctx.W, http.StatusOK, todos, "Returned all Todos for given user")
 	}
 }
 
-func (s *TodoServer) handleGetTodo() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		params := mux.Vars(r)
+func (s *TodoServer) handleGetTodo() api.HandlerFunc {
+	return func(ctx *api.CTX, next func()) {
+		params := mux.Vars(ctx.R)
 		id, _ := strconv.ParseInt(params["id"], 10, 0)
 
 		todo := s.db.GetTodoByID(int(id))
 
-		api.SendResponse(w, http.StatusOK, todo, "Returned Todo with given id")
+		api.SendResponse(ctx.W, http.StatusOK, todo, "Returned Todo with given id")
 	}
 }
 
-func (s *TodoServer) handleCreateTodo() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
+func (s *TodoServer) handleCreateTodo() api.HandlerFunc {
+	return func(ctx *api.CTX, next func()) {
 		var todo TodoEntry
-		json.NewDecoder(r.Body).Decode(&todo)
+		json.NewDecoder(ctx.R.Body).Decode(&todo)
 
 		// Insert
 		created, err := s.db.InsertTodo(todo)
 		if err != nil {
-			api.SendResponse(w, http.StatusInternalServerError, nil, "An error occured while creating Todo")
+			api.SendResponse(ctx.W, http.StatusInternalServerError, nil, "An error occured while creating Todo")
 			return
 		}
 
-		api.SendResponse(w, http.StatusOK, created, "Created a new Todo")
+		api.SendResponse(ctx.W, http.StatusOK, created, "Created a new Todo")
 	}
 }
 
-func (s *TodoServer) handleDeleteTodo() http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		params := mux.Vars(r)
+func (s *TodoServer) handleDeleteTodo() api.HandlerFunc {
+	return func(ctx *api.CTX, next func()) {
+		params := mux.Vars(ctx.R)
 		id, _ := strconv.ParseInt(params["id"], 10, 0)
 
 		err := s.db.DeleteTodo(int(id))
 		if err != nil {
-			api.SendResponse(w, http.StatusNotFound, nil, "An error occured while deleting Todo")
+			api.SendResponse(ctx.W, http.StatusNotFound, nil, "An error occured while deleting Todo")
 			return
 		}
 
 		body := map[string]string{
 			"message": "Todo has been removed",
 		}
-		api.SendResponse(w, http.StatusOK, body, "Deleted Todo with given ID")
+		api.SendResponse(ctx.W, http.StatusOK, body, "Deleted Todo with given ID")
 	}
 }
 
 func setRoutes(s *TodoServer) {
 	r := s.router
 
-	r.HandleFunc("/health", s.handleHealthCheck()).Methods("GET")
-	r.HandleFunc("/todos/{id:[0-9]+}", s.handleGetTodo()).Methods("GET")
-	r.HandleFunc("/todos/{id:[0-9]+}", s.handleDeleteTodo()).Methods("DELETE")
-	r.HandleFunc("/todos", s.handleGetTodos()).Methods("GET")
-	r.HandleFunc("/todos", s.handleCreateTodo()).Methods("POST")
+	r.HandleFunc(
+		"/health",
+		api.Handler(
+			s.handleHealthCheck(),
+		),
+	).Methods("GET")
+	r.HandleFunc(
+		"/todos/{id:[0-9]+}",
+		api.Handler(
+			s.handleGetTodo(),
+		),
+	).Methods("GET")
+	r.HandleFunc(
+		"/todos/{id:[0-9]+}",
+		api.Handler(
+			s.handleDeleteTodo(),
+		),
+	).Methods("DELETE")
+	r.HandleFunc(
+		"/todos",
+		api.Handler(
+			s.handleGetTodos(),
+		),
+	).Methods("GET")
+	r.HandleFunc(
+		"/todos",
+		api.Handler(
+			s.handleCreateTodo(),
+		),
+	).Methods("POST")
 }
